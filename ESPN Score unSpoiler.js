31b18320f5736a1a5a510631a8d71b6dda6784a5
Tyler Shivers <jtshiv@users.noreply.github.com>
2022-05-02 17:08:03 -0400

    Added jQuery requirement

--- ESPN Score unSpoiler.js
+++ ESPN Score unSpoiler.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         ESPN Score unSpoiler
 // @namespace    https://github.com/jtshiv/Tampermonkey
-// @version      0.7
+// @version      0.8
 // @description  Remove scores and spoilers from espn.com
 // @updateURL    https://github.com/jtshiv/Tampermonkey/raw/main/ESPN%20Score%20unSpoiler.js
 // @author       jtshiv
 // @match        https://www.espn.com/
 // @include      https://www.espn.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=espn.com
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
