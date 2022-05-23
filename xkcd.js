d252e086d14b1ccfa0b3fc8ba0f7b7bf51789288
Tyler Shivers <shiversjt@gmail.com>
2022-05-23 09:24:26 -0400

    feat: increment version

--- xkcd.js
+++ xkcd.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         xkcd
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       jtshiv
 // @updateURL	 https://raw.githubusercontent.com/jtshiv/Tampermonkey/main/xkcd.js
