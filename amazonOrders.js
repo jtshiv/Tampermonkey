a76c7c61e25f9ec7c919561ad3a1e44852928b74
Tyler Shivers <shiversjt@gmail.com>
2022-06-15 11:57:56 -0400

    initial commit amazonOrders.js

---
+++ amazonOrders.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Amazon Orders
+// @namespace    http://tampermonkey.net/
+// @version      0.0
+// @description  Mobilize Amazon Order Page
+// @updateURL    https://raw.githubusercontent.com/jtshiv/Tampermonkey/main/amazonOrders.js
+// @supportURL	 https://github.com/jtshiv/Tampermonkey/issues/new
+// @author       jtshiv
+// @include      https://www.amazon.com/gp/your-account/order-history/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
