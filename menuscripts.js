2697188d8beefe2a9553a7d93c54e101787261d0
tyler <shiversjt@gmail.com>
2024-03-05 11:15:27 -0500

    menu: bible html replace all multispaces with single space

--- menuscripts.js
+++ menuscripts.js
@@ -208,6 +208,7 @@ function bibleGateRemVerse(getHtml=false){
         var day = prompt("What day?");
         if (day == undefined){return};
         var text = "<h1>Day " + day + "</h1>\n" + document.querySelector('.passage-box').innerText;
+        text = text.replaceAll(/\s+/g, ' '); // to fix BG's weird spacing on poems
         text = text.replaceAll("\n","<br>");
 
          // Convert day to a 3-digit number with leading zeros for file
