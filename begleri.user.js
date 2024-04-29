cd2d70663d17c0270f2ac18167321e8a8c86442f
tyler <shiversjt@gmail.com>
2024-04-29 12:20:49 -0400

    create begleri file

---
+++ begleri.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Begleri Beta
+// @namespace    http://tampermonkey.net/
+// @version      2024.04.29.001
+// @match        https://begleritricks.com/progression-ladder
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=begleritricks.com
+// @grant        none
+// @run-at       document-start
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
