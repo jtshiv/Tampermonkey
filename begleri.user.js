0e4dd9e75beeef8fc09b5cce2f195a7c7e45cb5e
tyler <shiversjt@gmail.com>
2024-04-29 13:02:43 -0400

    add fn to wait for api. add css
    made promise to wait until myapi was loaded by utilities. once it is, it loads css to change main panel's max width and hide the right panel of trick index

--- begleri.user.js
+++ begleri.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Begleri Beta
 // @namespace    http://tampermonkey.net/
-// @version      2024.04.29.001
+// @version      2024.04.29.002
 // @match        https://begleritricks.com/progression-ladder
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=begleritricks.com
 // @grant        none
@@ -9,8 +9,35 @@
 // @noframes
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
-    // Your code here...
+    // load script info
+    var info = GM_info;
+    console.log(info.script.name + " has started");
+    
+    let begleriStyle = `
+        .mainhere h1, .mainhere h2, .mainhere h3, .mainhere p {
+            max-width: 100% !important;
+        }
+        #rightside {
+            display: none !important;
+        }`;
+
+    // waits until myapi is loaded by utilities
+    //      this is needed for adding the max width style
+    var apiPromise = new Promise(async resolve=>{
+        while (typeof myapi !== 'object') {
+            //console.log('api not object');
+            await new Promise(r => setTimeout(r, 100))
+        };
+        resolve(true);
+    });
+    // after myapi exists
+    apiPromise.then(()=>{
+        //console.log("api exists")
+        myapi.addStyle(begleriStyle);
+    });
+
+
 })();
